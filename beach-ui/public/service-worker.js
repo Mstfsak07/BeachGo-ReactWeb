8fb93129eab857e1cb3e77d847fa7606049e8ef9
Mustafa sak <mustafasak07@gmail.com>
2026-04-11 12:05:08 +0300

    Stabilize PWA service worker caching

--- beach-ui/public/service-worker.js
+++ beach-ui/public/service-worker.js
@@ -1,22 +1,75 @@
-const CACHE_NAME = 'beachgo-v1';
-const urlsToCache = [
+const CACHE_VERSION = 'beachgo-shell-v2';
+const APP_SHELL_FILES = [
   '/',
   '/index.html',
-  '/static/js/main.chunk.js',
-  '/static/css/main.chunk.css',
-  '/manifest.json'
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png',
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(APP_SHELL_FILES))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_VERSION)
+          .map((key) => caches.delete(key))
+      )
+    )
   );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+  const isSameOrigin = requestUrl.origin === self.location.origin;
+  const isNavigationRequest = event.request.mode === 'navigate';
+  const isStaticAsset =
+    isSameOrigin &&
+    (requestUrl.pathname.startsWith('/static/') ||
+      APP_SHELL_FILES.includes(requestUrl.pathname));
+
+  if (isNavigationRequest) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put('/index.html', responseClone));
+          return response;
+        })
+        .catch(async () => {
+          const cache = await caches.open(CACHE_VERSION);
+          return cache.match('/index.html');
+        })
+    );
+    return;
+  }
+
+  if (isStaticAsset) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request).then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, responseClone));
+          return response;
+        });
+      })
+    );
+  }
 });
